ff30ce11ca63792c8b069772b5da2182acda71e4
Hoang Dang <118740300+Pandadanh@users.noreply.github.com>
2025-03-12 19:51:31 +0700

    12

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { WsAdapter } from '@nestjs/platform-ws';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
+  const port = 3001;
 
   // Cho phép WebSocket hoạt động
   app.useWebSocketAdapter(new WsAdapter(app));
